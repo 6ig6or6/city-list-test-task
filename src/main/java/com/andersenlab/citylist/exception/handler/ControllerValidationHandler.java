fba193ac1d9050ee47262971f06037f81fcbcd8a
Ihar Kalesnikau <i.kalesnikau@andersenlab.com>
2023-02-16 17:17:16 +0100

    fix: added ControllerValidationHandler to send correct error messages in case of invalid arguments

---
+++ src/main/java/com/andersenlab/citylist/exception/handler/ControllerValidationHandler.java
@@ -0,0 +1,26 @@
+package com.andersenlab.citylist.exception.handler;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerValidationHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
